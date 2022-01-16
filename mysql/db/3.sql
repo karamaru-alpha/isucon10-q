c304191483abc2d66d622befacd8f9fca60154fd
Karacky <mrnk3078@gmail.com>
2022-01-16 14:22:58 +0900

    point

--- mysql/db/3.sql
+++ mysql/db/3.sql
@@ -1,3 +1,5 @@
 ALTER TABLE isuumo.estate ADD COLUMN geom POINT;
-UPDATE isuumo.estate SET geom=CONCAT('POINT(', latitude, ' ', longitude, ')');
+
+UPDATE isuumo.estate SET geom=POINT(latitude longitude);
+
 ALTER TABLE isuumo.estate MODIFY COLUMN geom POINT NOT NULL DEFAULT '' INVISIBLE, ADD SPATIAL INDEX(geom);
